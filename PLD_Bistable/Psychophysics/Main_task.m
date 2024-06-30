8615f4d7d20caee3e78d97b06ec3227b6d279a5f
Miladyekani <49733770+Miladyekani@users.noreply.github.com>
2024-06-30 18:32:48 +0330

    main task

--- PLD_Bistable/Psychophysics/Main_task.m
+++ PLD_Bistable/Psychophysics/Main_task.m
@@ -4,7 +4,11 @@ clc; clear ;
 % struct for saving the data 
 participantName    = input('Enter the participant name: ', 's');
 Type_of_experiment = input('type of experiment: ');
-Type_of_experiment = input('block : ');
+Block_number = input('block : ');
+if Block_number>10
+    msg = 'Block_number cant be more than 10';
+    error(msg)
+end 
 %%
 currentDateTime = datetime('now');
 DateTime = datestr(currentDateTime, 'yyyy-mm-dd-HH-MM-SS');
@@ -26,9 +30,37 @@ lineWidth = 4;lineLength = 20;lineColor = [255 255 255];
 Screen('BlendFunction', window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); % ??? dont know 
 [screenXpixels, screenYpixels] = Screen('WindowSize', window);
 ifi = Screen('GetFlipInterval', window);% Query the frame duration
+%%
+% % Inform the subject about the start of the task 
+space  =     KbName('space') ;
+image_start = imread('stimuli_set\ready.jpg') ;
+image_start =  imresize(image_start, 0.5);
+Task_initation   = Screen('MakeTexture', window,image_start); 
+Screen('DrawTexture', window, Task_initation);
+vbl  = Screen('Flip', window);
+        while 1
+            [a,b,keyCode] = KbCheck;
+            if any(keyCode(space))
+                Screen('Flip', window);
+                break
+            end
+        end
+% % test the termination message here 
+
+
+
 %% creating all textures 
 % make texxtures that are not realated to the stimuli like response screen 
-Response_texture = Screen('MakeTexture', window,imread('stimuli_set\Resp_Stimuli_for_PLD.jpg')); 
+image_response = imread('stimuli_set\Resp_PLD.jpg'); 
+image_response =  imresize(image_response, 0.3);
+Response_texture = Screen('MakeTexture', window,image_response); 
+% make low and high response texture 
+image_response_emotion = imread('stimuli_set\Resp_Emotion.jpg'); 
+image_response_emotion =  imresize(image_response_emotion, 0.3);
+Response_texture_emotion = Screen('MakeTexture', window,image_response_emotion);
+image_end =  imread('stimuli_set\finish.jpg') ;
+image_end =  imresize(image_end, 0.5);
+Task_termination   = Screen('MakeTexture', window,image_end);
 % make textures that are related to the walking stimulus
 loaded_frame     = 500;
 Texture.PLD      = Video_Text_Maker ('stimuli_set\PLD_BS'   ,loaded_frame,0,window);
@@ -39,7 +71,7 @@ Im_path_happy    = 'stimuli_set\happy_face_1';
 Im_path_angry    = 'stimuli_set\angry_face_1';
 
 
-for i=1:50 
+for i=1:34 
     if i<10
         image_nutre = imread(fullfile('stimuli_set\happy_face_1', strcat ( '__face005__0000', num2str(1),'.jpg')));
         textures_nutre{i}                 =  Screen('MakeTexture', window, image_nutre ) ;
@@ -47,19 +79,12 @@ for i=1:50
         textures_happy{i}                 =  Screen('MakeTexture', window, image_happy ) ;
         image_angry                       =  imread(fullfile(Im_path_angry, strcat ( '__face005__0000', num2str(i),'.jpg'))) ;
         textures_angry{i}                 =  Screen('MakeTexture', window, image_angry  ) ;
-     elseif i<35 && i>9
+     elseif  i>=10
         image_nutre = imread(fullfile('stimuli_set\happy_face_1', strcat ( '__face005__0000', num2str(1),'.jpg')));
         textures_nutre {i}                =  Screen('MakeTexture', window, image_nutre ) ;
         image_happy                       =  imread(fullfile(Im_path_happy, strcat ( '__face005__000', num2str(i),'.jpg'))) ;  
         textures_happy{i}                 =  Screen('MakeTexture', window, image_happy  ) ;
         image_angry                       =  imread(fullfile(Im_path_angry, strcat ( '__face005__000', num2str(i),'.jpg'))) ; 
-        textures_angry{i}                 =  Screen('MakeTexture', window, image_angry ) ;
-    elseif  i>=35 
-        image_nutre = imread(fullfile('stimuli_set\happy_face_1', strcat ( '__face005__0000', num2str(1),'.jpg')));
-        textures_nutre  {i}               =  Screen('MakeTexture', window, image_nutre ) ;
-        image_happy                       =  imread(fullfile(Im_path_happy, strcat ( '__face005__00035','.jpg'))) ;      
-        textures_happy{i}                 =  Screen('MakeTexture', window, image_happy  ) ;
-        image_angry                       =  imread(fullfile(Im_path_angry, strcat ( '__face005__00035','.jpg'))) ;
         textures_angry{i}                 =  Screen('MakeTexture', window, image_angry ) ;
     end
 end 
@@ -73,15 +98,15 @@ end
 Up    = KbName('uparrow')   ; Down  = KbName('downarrow') ; Right = KbName('rightarrow'); 
 Left  = KbName('leftarrow') ; space  =     KbName('space') ;
 %% setuo the others of the trails 
-Chunke_number =5 ;
+Chunke_number =7 ;
 if Chunke_number < 5
     error ('Chunke_number should be more than 4');
 end
 Participant_responded = 0 ;
-interval_for_modulation = repmat([5, 6], 1, Chunke_number);
+interval_for_modulation = repmat([3, 4], 1, Chunke_number);
 trial_number = sum(interval_for_modulation) ; 
 interval_for_modulation = interval_for_modulation(randperm(length(interval_for_modulation)));
-Modulation_type = repmat([1, 2,3,4,5], 1, length(interval_for_modulation)/5);
+Modulation_type = repmat([1, 2,3,4,5,6,7], 1, length(interval_for_modulation)/7);
 interval_for_modulation = interval_for_modulation(randperm(length(interval_for_modulation)));
 Modulation_type = Modulation_type(randperm(length(Modulation_type))) ; 
 for i=2:length(interval_for_modulation)
@@ -98,14 +123,38 @@ desiredX = centerX - picWidth/2; % X position
 desiredY = centerY + distanceFromCenter; % Y position
 % Calculate the coordinates to place the picture at the desired position
 positionRect = [desiredX, desiredY, desiredX + picWidth, desiredY + picHeight];
+% Define button positions
+buttonWidth = 400;
+buttonHeight = 400;
+screenRect = Screen('Rect', window);
+leftButtonRect = [screenRect(3) / 4 - buttonWidth, screenRect(4) / 2 - buttonHeight / 2, screenRect(3) / 4, screenRect(4) / 2 + buttonHeight / 2];
+rightButtonRect = [3 * screenRect(3) / 4, screenRect(4) / 2 - buttonHeight / 2, 3 * screenRect(3) / 4 + buttonWidth, screenRect(4) / 2 + buttonHeight / 2];
+
+
 %% presenting the stimuli and getting the responses 
 modulation_trail = 0  ; 
+participant_score = 0 ; 
 chunk_n = 1 ; 
 last_trail = interval_for_modulation(end) ;
+
+
 for trail=1 :trial_number 
         resp =0 ;  
          % terminate the session if the last trail has been reached 
         if trail == last_trail
+            Screen('DrawTexture', window, Task_termination);
+            Screen('Flip', window);
+            Screen('TextFont', window, 'Arial');
+            Screen('TextSize', window, 50);
+            Screen('DrawText', window, num2str(participant_score), centerX, centerY, [255 0 0]);
+            Screen('Flip', window);
+        while 1
+            [a,b,keyCode] = KbCheck;
+            if any(keyCode(space))
+                Screen('Flip', window);
+                break
+            end
+        end
             % save(fil_name,'Info.mat');
             sca;
             break;
@@ -127,14 +176,27 @@ for trail=1 :trial_number
                 texture     =  textures_nutre;
                 info.trail_type {trail} = 'Nutre';
                 movement_stimulus = 0; 
+                High_type = -1; 
             elseif Modulation_type(chunk_n) == 4
                 texture     = textures_happy ;
-                info.trail_type {trail} = 'Happy';
+                info.trail_type {trail} = 'Happy_high';
                 movement_stimulus = 0; 
+                High_type = 1; 
             elseif Modulation_type(chunk_n) == 5
                 texture     = textures_angry ;
-                info.trail_type {trail} = 'Angry';
-                movement_stimulus = 0; 
+                info.trail_type {trail} = 'Angry_high';
+                movement_stimulus = 0;
+                High_type =1; 
+            elseif Modulation_type(chunk_n) == 6
+                texture     = textures_happy ;
+                info.trail_type {trail} = 'Happy_low';
+                movement_stimulus = 0;
+                High_type = 0; 
+            elseif Modulation_type(chunk_n) == 7
+                texture     = textures_angry ;
+                info.trail_type {trail} = 'Angry_low';
+                movement_stimulus = 0;
+                High_type = 0; 
             end
         else
             modulation_trail = 0  ;
@@ -144,13 +206,21 @@ for trail=1 :trial_number
         end
       
         if movement_stimulus == 1
-            start_frame = randi([2, 20], 1, 1); ; 
+            start_frame = randi([2, 20], 1, 1); 
             end_frame = 200; 
             IFW = 0.001 ; 
-        else
+        elseif movement_stimulus == 0 && High_type == 0
             start_frame = 1 ;
-            end_frame = 49 ; 
+            end_frame = 22+Block_number ; 
             IFW = 0.03 ; 
+        elseif movement_stimulus ==0 && High_type == 1
+            start_frame = 1 ;
+            end_frame = 33 ;
+            IFW = 0.03 ;
+        elseif movement_stimulus ==0 && High_type == -1
+            start_frame = 1 ;
+            end_frame = 33 ;
+            IFW = 0.03 ;
         end
         
    %present the white noise if modulation trial is reached
@@ -158,14 +228,13 @@ for trail=1 :trial_number
         N = 44100 *1.67; % Number of samples
         white_noise = randn(N,1);
         sound_obj = audioplayer(white_noise, 44100);
-        play(sound_obj);
     for i=1 : 100
         Screen('DrawTexture', window, white_noise_texture{i}, [], [], 0);
         play(sound_obj);
         Screen('Flip', window);
     end
     end 
-
+    %show the start of the task 
     % present the fixation cross
     Screen('DrawLine', window, lineColor, centerX, centerY-lineLength/2, centerX, centerY+lineLength/2, lineWidth);
     Screen('DrawLine', window, lineColor, centerX-lineLength/2, centerY, centerX+lineLength/2, centerY, lineWidth);
@@ -184,6 +253,14 @@ for trail=1 :trial_number
        
          WaitSecs(IFW);
     end
+    %  wait if we have persented face stimulus 
+    if movement_stimulus ~= 1 && High_type ==0
+         WaitSecs(2.5+8*ifi);
+    elseif movement_stimulus ~= 1 && High_type ==1
+         WaitSecs(2.5);
+    elseif movement_stimulus ~= 1 && High_type ==-1
+        WaitSecs(2.5);
+    end 
 
     if movement_stimulus == 1
         Screen('Flip',window)
@@ -200,9 +277,61 @@ for trail=1 :trial_number
                 break
             end
         end
+    elseif movement_stimulus == 0 &&  High_type ~= -1
+        Screen('Flip',window)
+        WaitSecs(0.5);
+        T_0 = GetSecs() ;
+        while 1
+            [a,b,keyCode] = KbCheck;
+            Screen('DrawTexture', window, Response_texture_emotion)
+            Screen('Flip',window)
+            if any(keyCode(Right)) ||any(keyCode(Left))
+                info.Reaction_Time{trail} = GetSecs() - T_0 ;
+                info.Respons{trail}  = KbName(keyCode);
+                Beeper(1000,10);
+                disp(KbName(keyCode));
+                break
+            end
+        end
+        if  strcmp(KbName(keyCode),'LeftArrow')&& High_type == 1
+            % correct reponse 
+            Screen('TextFont', window, 'Arial');
+            Screen('TextSize', window, 50);
+            Screen('DrawText', window, 'True', centerX, centerY, [0 255 0]);
+            Screen('Flip', window);
+            participant_score = participant_score+ 10 ; 
+            WaitSecs(2);
+        elseif strcmp(KbName(keyCode),'RightArrow')&& High_type == 1
+            % uncorrect reponse
+            Screen('TextFont', window, 'Arial');
+            Screen('TextSize', window, 50);
+            Screen('DrawText', window, 'False', centerX, centerY, [255 0 0]);
+            Screen('Flip', window);
+            participant_score = participant_score- 10 ; 
+            WaitSecs(2);
+        elseif strcmp(KbName(keyCode),'RightArrow')&& High_type == 0
+            % correct reponse
+            Screen('TextFont', window, 'Arial');
+            Screen('TextSize', window, 50);
+            Screen('DrawText', window, 'True', centerX, centerY, [0 255 0]);
+            Screen('Flip', window);
+            participant_score = participant_score+ 10 ; 
+            WaitSecs(2);
+        elseif strcmp(KbName(keyCode),'LeftArrow')&& High_type == 0
+            % uncorrect reponse
+            Screen('TextFont', window, 'Arial');
+            Screen('TextSize', window, 50);
+            Screen('DrawText', window, 'False', centerX, centerY, [255 0 0]);
+            Screen('Flip', window);
+            participant_score = participant_score- 10 ; 
+            WaitSecs(2);
+        end
+        
+
     end
+    
+ 
 end 
-sca ; 
 
 
 
